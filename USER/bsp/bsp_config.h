a60b9af5c549ee2def9f73a57efae9c5e5c6b38e
zhangyuhu <zhangyuhu@dding.cn>
2018-03-07 13:57:05 +0800

    Updata led drive.

--- USER/bsp/bsp_config.h
+++ USER/bsp/bsp_config.h
@@ -1,7 +1,7 @@
 #ifndef __BSP_CONFIG_H
 #define __BSP_CONFIG_H
 
-
+#define LED_RCC                                   (RCC_APB2Periph_GPIOC)
 #define LED_PIN                                   (GPIO_Pin_13)
 #define LED_PORT                                  (GPIOC)
 
--- USER/bsp/led/led.c
+++ USER/bsp/led/led.c
@@ -11,7 +11,7 @@
 void LED_Init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
-    RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC , ENABLE);
+    RCC_APB2PeriphClockCmd(LED_RCC , ENABLE);
     GPIO_InitStructure.GPIO_Pin = LED_PIN;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
